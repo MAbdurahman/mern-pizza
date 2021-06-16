ae0f7b57928d3d59383d49cfbd20836b105cd680
MAbdurahman <mdbdrrhm@gmail.com>
2021-06-16 13:00:59 -0500

    Make adjustments to client-side

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import {Container} from 'react-bootstrap';
+import Navbar from './components/Navbar';
+
+function App() {
+	return (
+		<>
+		<Navbar />
+		<main>
+			<Container>
+
+			</Container>
+		</main>
+		</>
+	);
+}
+
+export default App;
